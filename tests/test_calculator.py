2516e8e4fa5b9d14cc9b8e2af030293b30ae95ff
metanoia <meta3479@gmail.com>
2021-07-02 22:44:28 +0300

    version 1.0

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -4,9 +4,9 @@ Command line: python -m pytest tests/test_calculator.py
 """
 from math import sqrt
 
-import pytest
-
 from calculator import Calculator
+
+import pytest
 
 
 @pytest.fixture
@@ -46,60 +46,102 @@ def test_invalid_methods_values(calculator, x, y, ex):
         calculator.root(x)
 
 
-def test_add_method(calculator: Calculator):
-    assert calculator.add(0, 0) == 0 + 0
-    assert calculator.add(55, 55) == 55 + 55
-    assert calculator.add(5, -6) == 5 + -6
-    assert calculator.add(-2, 10) == -2 + 10
-    assert calculator.add(-5, -5) == -5 + -5
+@pytest.mark.parametrize(
+    "x, y",
+    [
+        (0, 0),
+        (55, 55),
+        (5, -6),
+        (-2, 10),
+        (-5, -5),
+    ],
+)
+def test_add_method(calculator, x, y):
+    assert calculator.add(x, y) == x + y
 
 
-def test_subtract_method(calculator: Calculator):
-    assert calculator.subtract(0, 0) == 0 - 0
-    assert calculator.subtract(-5, -5) == -5 - -5
-    assert calculator.subtract(-15, 15) == -15 - 15
-    assert calculator.subtract(15, -15) == 15 - -15
-    assert calculator.subtract(10, 5) == 10 - 5
+@pytest.mark.parametrize(
+    "x, y",
+    [
+        (0, 0),
+        (-5, -5),
+        (-15, 15),
+        (15, -15),
+        (10, 5),
+    ],
+)
+def test_subtract_method(calculator, x, y):
+    assert calculator.subtract(x, y) == x - y
 
 
-def test_divide_method(calculator: Calculator):
+@pytest.mark.parametrize(
+    "x, y",
+    [
+        (-15, 15),
+        (15, -15),
+        (10, 100),
+        (100, 10),
+    ],
+)
+def test_divide_method(calculator, x, y):
+    assert calculator.divide(x, y) == x / y
     with pytest.raises(ZeroDivisionError):
         assert calculator.divide(0, 0)
-    assert calculator.divide(-15, 15) == -15 / 15
-    assert calculator.divide(15, -15) == 15 / -15
-    assert calculator.divide(10, 100) == 10 / 100
-    assert calculator.divide(100, 10) == 100 / 10
 
 
-def test_multiply_method(calculator: Calculator):
-    assert calculator.multiply(0, 0) == 0 * 0
-    assert calculator.multiply(-10, 2) == -10 * 2
-    assert calculator.multiply(2, -10) == 2 * -10
-    assert calculator.multiply(2, 2) == 2 * 2
-    assert calculator.multiply(-5, -5) == -5 * -5
+@pytest.mark.parametrize(
+    "x, y",
+    [
+        (0, 0),
+        (-10, 2),
+        (2, -10),
+        (2, 2),
+        (-5, -5),
+    ],
+)
+def test_multiply_method(calculator, x, y):
+    assert calculator.multiply(x, y) == x * y
 
 
-def test_mod_method(calculator: Calculator):
+@pytest.mark.parametrize(
+    "x, y",
+    [
+        (-300, 90),
+        (300, 90),
+        (7, 100),
+        (100, 7),
+    ],
+)
+def test_mod_method(calculator, x, y):
+    assert calculator.mod(x, y) == x % y
     with pytest.raises(ZeroDivisionError):
         assert calculator.mod(0, 0)
-    assert calculator.mod(-300, 90) == -300 % 90
-    assert calculator.mod(300, 90) == 300 % 90
-    assert calculator.mod(7, 100) == 7 % 100
-    assert calculator.mod(100, 7) == 100 % 7
 
 
-def test_power_method(calculator: Calculator):
-    assert calculator.power(2, 2) == 2 ** 2
-    assert calculator.power(5, 0) == 5 ** 0
-    assert calculator.power(1, 1) == 1 ** 1
-    assert calculator.power(0, 0) == 0 ** 0
-    assert calculator.power(2, 8) == 2 ** 8
+@pytest.mark.parametrize(
+    "x, y",
+    [
+        (2, 2),
+        (5, 0),
+        (1, 1),
+        (0, 0),
+        (2, 8),
+    ],
+)
+def test_power_method(calculator, x, y):
+    assert calculator.power(x, y) == x ** y
 
 
-def test_root_method(calculator: Calculator):
+@pytest.mark.parametrize(
+    "x",
+    [
+        (25),
+        (16),
+        (81),
+        (0),
+    ],
+)
+def test_root_method(calculator, x):
+    assert calculator.root(x) == sqrt(x)
     with pytest.raises(ValueError):
         assert calculator.root(-1)
-    assert calculator.root(25) == sqrt(25)
-    assert calculator.root(16) == sqrt(16)
-    assert calculator.root(81) == sqrt(81)
-    assert calculator.root(0) == sqrt(0)
